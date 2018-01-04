c33ffb5ca57eeb8e4670284dfd46669d30cfcbd8
acolix <34978487+acolix@users.noreply.github.com>
2018-01-04 11:52:10 -0800

    Update gamecode.js

--- gamecode.js
+++ gamecode.js
@@ -1,3 +1,10 @@
 can=document.createNewElement('canvas');
 document.body.addChildNode(can);
-
+context=canvas.getContext('2d')
+context.beginPath();
+context.moveTo(100, 400);
+context.lineTo(200, 400);
+context.lineWidth = 15;
+context.stroke();
+//function draw() {
+  
