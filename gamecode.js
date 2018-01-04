11d6855bc05c28441b01c694b1a576ab0ac4557a
acolix <34978487+acolix@users.noreply.github.com>
2018-01-04 12:18:09 -0800

    Update gamecode.js

--- gamecode.js
+++ gamecode.js
@@ -1,6 +1,9 @@
 var can=document.getElementById("myCanvas");
+can.setAttribute("height",600px);
+can.setAttribute("width",600px);
+
 //document.body.addChildNode(can);
-context=can.getContext('2d')
+context=can.getContext("2d")
 context.beginPath();
 context.moveTo(100, 400);
 context.lineTo(200, 400);
