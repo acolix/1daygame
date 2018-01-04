d2ae3526bd0dc122b7147657ad2b7f265cc9bcaf
acolix <34978487+acolix@users.noreply.github.com>
2018-01-04 12:48:15 -0800

    Update gamecode.js

--- gamecode.js
+++ gamecode.js
@@ -2,14 +2,15 @@ var can = document.getElementById('myCanvas');
 //var can = document.createElement('canvas');
 can.setAttribute("height","600px");
 can.setAttribute("width","600px");
-
 //document.body.addChildNode(can);
-var context= can.getContext('2d');
-context.beginPath();
-context.moveTo(100, 400);
-context.lineTo(200, 400);
-context.lineWidth = 15;
-context.strokeStyle="rgb(0,200,50)";
-context.stroke();
-//function draw() {
+function drawSlope (dh) {
+  var context= can.getContext('2d');
+  context.beginPath();
+  context.lineWidth = 15;
+  context.strokeStyle="rgb(0,200,50)";
+  context.moveTo(100, 400);
+  context.lineTo(200, 400);
+  context.quadraticCurveTo(400,200,250,120);
+  
+  context.stroke();
   
