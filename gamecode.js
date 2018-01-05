2043a2fc8dacb5b59f1ba6ae872233611cce9670
grantdennis123 <34985740+grantdennis123@users.noreply.github.com>
2018-01-04 22:15:36 -0500

    Update gamecode.js

--- gamecode.js
+++ gamecode.js
@@ -19,7 +19,7 @@ input.setAttribute("value","Enter Word");
 document.body.appendChild(form);
 form.appendChild(input);
 
-var submit = document.createElement("INPUT");
+var submit = document.createElement("submit");
 submit.setAttribute("type", "submit");
 submit.setAttribute("height","30px");
 submit.setAttribute("width","30px");
