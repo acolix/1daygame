fc375abf229939e8b55ff2ee926498fd802240ed
grantdennis123 <34985740+grantdennis123@users.noreply.github.com>
2018-01-04 22:25:43 -0500

    Update gamecode.js

--- gamecode.js
+++ gamecode.js
@@ -23,7 +23,7 @@ var submit = document.createElement("submit");
 submit.setAttribute("type","submit");
 submit.setAttribute("height","30px");
 submit.setAttribute("width","30px");
-var submit = form.elements[0].value;
+var x = form.elements[0].value;
 
 function drawSlope (dh) {
   var context= can.getContext('2d');
