99aaddc9f161d8be78106d0301fa47710bdc6e93
acolix <34978487+acolix@users.noreply.github.com>
2018-01-08 17:10:05 -0800

    Update gamecode.js

--- gamecode.js
+++ gamecode.js
@@ -219,7 +219,7 @@ function spikes (){
   var valleylength = wallx-rampx;
   var baselength = 10;
   var nspikes = valleylength/baselength;
-  for(i=1;i <= nspikes;i++1){;
+  for(i=1;i <= nspikes;i++){
     var x1 = rampx + baselength * (i-1);                         
     triangle(x1,y0, baselength, height);
     
